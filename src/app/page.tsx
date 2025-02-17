681bffc9f4748c26f5912783fa2d169a8e6d9d4f
Nehul Dalwala <nehul283.rejoice@gmail.com>
2025-02-17 11:07:59 +0530

    Refactor Home component layout and clean up formatting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,30 +2,28 @@ import Button from "@/components/common/button";
 import LoginBanner from "@/components/sections/loginBanner";
 import Link from "next/link";
 import Image from "next/image";
-const Vector = "/assets/icons/vec.png";
+const Vector = '/assets/icons/vec.png';
 export default function Home() {
   return (
     <div className="bg-primary h-screen md:max-w-[375px] md:mx-auto">
       <div className="h-[381px]">
-        <LoginBanner />
+        <LoginBanner/>
       </div>
       <div className="h-[calc(100vh-381px)] relative flex items-center">
         <div className="absolute left-[50%] top-[-40px] translate-x-[-50%]">
           <Image src={Vector} alt="Vector" width={100} height={100} priority />
         </div>
         <div className="px-5">
-          <div className="absolute inset-0 bg-[linear-gradient(transparent_95%,rgba(255,255,255,0.07)_5%),linear-gradient(90deg,transparent_95%,rgba(255,255,255,0.07)_5%)] bg-[length:40px_40px] opacity-20 pointer-events-none">
-            <h1 className="text-[40px] leading-[50px] text-white mb-8 font-heading text-center">
-              Manage Your GemFarm
-            </h1>
-            <p className="text-white opacity-[.55] text-sm font-medium text-center mb-20">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry
-            </p>
-            <Link href="/signin">
-              <Button text="Get Started" />
+          <h1 className="text-[40px] leading-[50px] text-white mb-8 font-heading text-center">
+          Manage Your GemFarm
+          </h1>
+          <p className="text-white opacity-[.55] text-sm font-medium text-center mb-20">
+            Lorem Ipsum is simply dummy text of the printing and 
+            typesetting industry
+          </p>
+            <Link href='/signin'>
+              <Button text="Get Started"/>
             </Link>
-          </div>
         </div>
       </div>
     </div>
