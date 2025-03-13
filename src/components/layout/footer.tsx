2c2dc455c0c2b8a09f9d0f93408593592e6a918b
mahesh <dev030.rejoice@gmail.com>
2025-03-13 18:14:57 +0530

    zindex issue fixed

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -5,17 +5,17 @@ export default function Footer() {
     const [toogle, setToogle] = useState(false);
     return (
         <div>
-            <div className='px-6 fixed bottom-5 w-full left-0 z-[99]'>
+            <div className='px-6 fixed bottom-5 w-full left-0 z-[9]'>
                 <div onClick={() => setToogle(!toogle)}>
                     <Button text="Watch List" green />
                 </div>
             </div>
             {
                 toogle && (
-                    <div className='fixed top-0 left-0 w-full h-full bg-modalBackdrop z-[999]'></div>
+                    <div className='fixed top-0 left-0 w-full h-full bg-modalBackdrop z-[99]'></div>
                 )
             }
-            <div className={`bg-white w-full bottom-0  fixed left-0 z-[9999] rounded-t-lg h-[calc(100dvh-100px)] transition-all duration-500 ease-in-out ${toogle ? 'translate-y-[0%]' : 'translate-y-[100%]'}`}>
+            <div className={`bg-white w-full bottom-0  fixed left-0 z-[999] rounded-t-lg h-[calc(100dvh-100px)] transition-all duration-500 ease-in-out ${toogle ? 'translate-y-[0%]' : 'translate-y-[100%]'}`}>
                 <div onClick={() => setToogle(false)} className='p-5'>
                     <Button text="Watch List" green />
                 </div>
