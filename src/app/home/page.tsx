c551480835ee68a7aaaf671db8f8e939d534aa33
Nehul Dalwala <nehul283.rejoice@gmail.com>
2025-03-13 16:27:21 +0530

    Enhance loading state display: add a centered loading spinner during user profile fetch

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -25,7 +25,13 @@ export default function page() {
     dispatch(fetchUserProfile() as any);
   }, [dispatch]);
 
-  if (loading) return <p>Loading...</p>;
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <div className="w-10 h-10 border-4 border-gray-300 border-t-primary rounded-full animate-spin"></div>
+      </div>
+    );
+  }
   const router = useRouter();
 
   const onClickFarm = () => {
