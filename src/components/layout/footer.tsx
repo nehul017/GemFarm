407f1ef8a40704517525b9cbf9c3d8883e038d44
Nehul Dalwala <nehul283.rejoice@gmail.com>
2025-03-24 17:41:38 +0530

    Center footer modal and adjust width for improved layout

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -20,12 +20,12 @@ export default function Footer() {
         </div>
       </div>
       {toogle && (
-        <div onClick={() => setToogle(false)} className="fixed top-0 left-0 w-full h-full bg-modalBackdrop z-[99]"></div>
+         <div onClick={() => setToogle(false)} className="fixed top-0 left-[50%] translate-x-[-50%] w-full h-full bg-modalBackdrop z-[99] mx-auto max-w-[380px]"></div>
       )}
       <div
-        className={`bg-white w-full bottom-0  fixed left-0 z-[999] rounded-t-lg h-[calc(100dvh-100px)] transition-all duration-500 ease-in-out ${
-           toogle ? "translate-y-[0%]" : "translate-y-[100%]"
-         }`}
+        className={`bg-white max-w-[380px] w-full bottom-0 left-[50%] translate-x-[-50%] mx-auto fixed left-0 z-[999] rounded-t-lg h-[calc(100dvh-100px)] transition-all duration-500 ease-in-out ${
+          toogle ? "translate-y-[0%]" : "translate-y-[100%]"
+        }`}
       >
         <div onClick={() => setToogle(false)} className="p-5">
           <Button
