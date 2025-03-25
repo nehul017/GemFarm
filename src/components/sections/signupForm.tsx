cb7c1c366449a4e2b70af3237d0fd63dad62d1d7
Nehul Dalwala <nehul283.rejoice@gmail.com>
2025-03-25 17:00:43 +0530

    Add loading indicator to Sign Up button in SignupForm

--- src/components/sections/signupForm.tsx
+++ src/components/sections/signupForm.tsx
@@ -96,7 +96,30 @@ export default function SignupForm() {
           />
         </div>
         <div className="pt-[30px]">
-          <Button green text="Sign Up" type="submit" disabled={loading} />
+          <Button green text="Sign Up" type="submit" disabled={loading}>
+            {loading ? (
+              <div className="flex items-center justify-center">
+                Sign Up...
+                <svg className="animate-spin h-5 w-5 mr-2" viewBox="0 0 24 24">
+                  <circle
+                    className="opacity-25"
+                    cx="12"
+                    cy="12"
+                    r="10"
+                    stroke="currentColor"
+                    strokeWidth="4"
+                  ></circle>
+                  <path
+                    className="opacity-75"
+                    fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4z"
+                  ></path>
+                </svg>
+              </div>
+            ) : (
+              "Sign Up"
+            )}
+          </Button>
         </div>
         <div className="absolute bottom-5 w-full left-0">
           <div className="px-5">
