39fe24bafdbb2c018e4dee157d324b77b2460ae9
Nehul Dalwala <nehul283.rejoice@gmail.com>
2025-03-27 12:54:38 +0530

    Add overflow control to body based on footer toggle state

--- src/components/layout/footer.tsx
+++ src/components/layout/footer.tsx
@@ -1,11 +1,18 @@
 "use client";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Button from "../common/button";
 import CropsList from "../common/CropsList";
 const DownIcon = "/assets/icons/down.svg";
 
 export default function Footer() {
   const [toogle, setToogle] = useState(false);
+  useEffect(() => {
+    if (toogle) {
+      document.body.style.overflow = 'hidden'
+    } else {
+      document.body.style.overflow = ''
+    }
+  }, [toogle])
   return (
     <div>
       <div className="fixed bottom-5 left-1/2 transform -translate-x-1/2 z-[9] max-w-[380px] w-full mx-auto">
@@ -19,16 +26,10 @@ export default function Footer() {
           />
         </div>
       </div>
-      {toogle && (
-        <div
-          onClick={() => setToogle(false)}
-          className="fixed top-0 left-[50%] translate-x-[-50%] w-full h-full bg-modalBackdrop z-[99] mx-auto max-w-[380px]"
-        ></div>
-      )}
+
       <div
-        className={`bg-white max-w-[380px] w-full bottom-0 left-[50%] translate-x-[-50%] mx-auto fixed  z-[999] rounded-t-lg h-[calc(100dvh-100px)] transition-all duration-500 ease-in-out ${
-          toogle ? "translate-y-[0%]" : "translate-y-[100%]"
-        }`}
+        className={`bg-white max-w-[380px] w-full bottom-0 left-[50%] translate-x-[-50%] mx-auto fixed  z-[999] rounded-t-lg h-[calc(100dvh-100px)] transition-all duration-500 ease-in-out ${toogle ? "translate-y-[0%]" : "translate-y-[100%]"
+          }`}
       >
         <div onClick={() => setToogle(false)} className="p-5">
           <Button
