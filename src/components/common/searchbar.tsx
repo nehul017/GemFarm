13e403556df30337897e67691bd39d8e2d8fe2ee
Nehul Dalwala <nehul283.rejoice@gmail.com>
2025-04-11 12:49:27 +0530

    Add clear button functionality to Searchbar component

--- src/components/common/searchbar.tsx
+++ src/components/common/searchbar.tsx
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import SearchIcon from "@/icons/SearchIcon";
-import VoiceIcon from "@/icons/voiceIcon";
+import CloseIcon from "@/icons/closeIcon";
 interface SearchbarProps {
   onSearch: (value: string) => void;
 }
@@ -13,19 +13,28 @@ export default function Searchbar({ onSearch }: SearchbarProps) {
     setSearchText(value);
     onSearch(value);
   };
+  const handleClear = () => {
+    setSearchText("");
+    onSearch("");
+  };
 
   return (
     <div className="relative w-full">
       <input
-        className="pl-5 pr-10 text-sm bg-inputBackground placeholder:text-black placeholder:opacity-[0.45] rounded-full placeholder:text-sm outline-none h-[50px] w-full text-black font-normal border border-solid border-black"
+        className="pl-10 pr-10 text-sm bg-inputBackground placeholder:text-black placeholder:opacity-[0.45] rounded-full placeholder:text-sm outline-none h-[50px] w-full text-black font-normal border border-solid border-black"
         type="text"
         value={searchText}
         onChange={handleChange}
         placeholder="Search Crops..."
       />
-      <div className="absolute top-[50%] translate-y-[-50%] right-3 flex items-center cursor-pointer">
+      <div className="absolute top-[50%] translate-y-[-50%] left-3 flex items-center cursor-pointer">
         <SearchIcon />
       </div>
+      {searchText && (
+        <div className="absolute top-[50%] translate-y-[-50%] right-3 flex items-center cursor-pointer" onClick={handleClear}>
+          <CloseIcon />
+        </div>
+      )}
     </div>
   );
 }
