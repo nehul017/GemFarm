a38e7f9143cebd4c4ade1bfa0f6d92247d897ea3
Nehul Dalwala <nehul283.rejoice@gmail.com>
2025-05-02 18:29:12 +0530

    fix: Update className in CustomDatePicker for improved styling and consistency

--- src/components/common/CustomDatePicker.tsx
+++ src/components/common/CustomDatePicker.tsx
@@ -48,7 +48,7 @@ const CustomDatePicker: React.FC<CustomDatePickerProps> = ({
           toggleCalendarOnIconClick
           //   style={{ display: "" }}
           dateFormat="dd/MM/yyyy"
-          className={`h-[50px] px-3 py-2 border border-[#E6E6E6] bg-[#f9f9f9] text-[#111827] text-sm rounded-md focus:outline-none focus:ring-2 focus:ring-[#10B981] focus:border-[#36BA7E] w-full ${error ? "border-red-500 focus:ring-red-500 focus:border-red-500" : ""}`} 
+          className={`h-[50px] !px-3 py-2 border border-[#E6E6E6] bg-[#f9f9f9] text-[#111827] text-base placeholder:text-base rounded-md focus:outline-none focus:ring-2 focus:ring-[#10B981] focus:border-[#36BA7E] w-full ${error ? "border-red-500 focus:ring-red-500 focus:border-red-500" : ""}`} 
           calendarClassName="rounded-md shadow-md border border-[#E5E5E5]"
         />
       {/* </div> */}
