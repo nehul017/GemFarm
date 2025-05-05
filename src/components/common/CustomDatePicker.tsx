08f36c5fbb46a5df83588239ac8efdf716a9fe39
Nehul Dalwala <nehul283.rejoice@gmail.com>
2025-05-05 11:32:06 +0530

    refactor: Simplify CustomInput component in CustomDatePicker for better readability

--- src/components/common/CustomDatePicker.tsx
+++ src/components/common/CustomDatePicker.tsx
@@ -20,16 +20,22 @@ const CustomDatePicker: React.FC<CustomDatePickerProps> = ({
   error,
   required = false,
 }) => {
-  const inputClassName = `
-  h-[50px] px-3 py-2 
-  ${
-    error
-      ? "border-red-500 focus:ring-red-500 focus:border-red-500"
-      : "border-[#E6E6E6] focus:ring-[#10B981] focus:border-[#36BA7E]"
-  } 
-  bg-[#f9f9f9] text-[#111827] text-sm 
-  rounded-md w-full focus:outline-none
-`;
+
+  const CustomInput = React.forwardRef<HTMLInputElement, any>(
+    ({ value, onClick, placeholder, error }: any, ref) => (
+      <input
+        type="text"
+        readOnly
+        value={value}
+        onClick={onClick}
+        placeholder={placeholder}
+        ref={ref}
+        className={`h-[50px] py-2 px-3 border ${
+          error ? "border-red-500 focus:ring-red-500 focus:border-red-500" : "border-[#E6E6E6] focus:ring-[#10B981] focus:border-[#36BA7E]"
+        } bg-[#f9f9f9] text-[#111827] text-base placeholder:text-base rounded-md focus:outline-none focus:ring-2 w-full`}
+      />
+    )
+  );
 
   return (
     <div>
@@ -41,15 +47,14 @@ const CustomDatePicker: React.FC<CustomDatePickerProps> = ({
       )}
       {/* <div className={`relative ${error ? "border-red-500 focus:ring-red-500 focus:border-red-500" :""}`}> */}
         <DatePicker
-          showIcon
-          selected={selectedDate}
-          onChange={onChange}
-          placeholderText={placeholder}
-          toggleCalendarOnIconClick
-          //   style={{ display: "" }}
-          dateFormat="dd/MM/yyyy"
-          className={`h-[50px] !px-3 py-2 border border-[#E6E6E6] bg-[#f9f9f9] text-[#111827] text-base placeholder:text-base rounded-md focus:outline-none focus:ring-2 focus:ring-[#10B981] focus:border-[#36BA7E] w-full ${error ? "border-red-500 focus:ring-red-500 focus:border-red-500" : ""}`} 
-          calendarClassName="rounded-md shadow-md border border-[#E5E5E5]"
+        showIcon
+        selected={selectedDate}
+        onChange={onChange}
+        placeholderText={placeholder}
+        toggleCalendarOnIconClick
+        dateFormat="dd/MM/yyyy"
+        calendarClassName="rounded-md shadow-md border border-[#E5E5E5]"
+        customInput={<CustomInput error={error} placeholder={placeholder} />}
         />
       {/* </div> */}
       {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
